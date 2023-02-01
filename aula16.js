0e7f6cf7fd1253dc971aa4962fd51707c0e2fc3c
CelitoLK <celitolk@outlook.com>
2023-01-31 22:08:03 -0300

    Primeiro Commit

---
+++ aula16.js
@@ -0,0 +1,14 @@
+const nome = 'Celito';
+const sobrenome = 'k';
+const idade = 33;
+const peso = 68;
+const altura = 1.71;
+let imc;
+let anoNascimento;
+
+imc = peso / (altura**2);
+anoNascimento = 2022 - idade;
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos e pesa ${peso}kg`);
+console.log(`tem ${altura}m de altura e seu IMC é de ${imc}`);
+console.log(`${nome} nasceu no ano de  ${anoNascimento}`)
